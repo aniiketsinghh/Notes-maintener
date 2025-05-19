0a7c2591194b24c966aa43f0233fb137769fa613
aniketsingh <aniketsingh2004@gmail.com>
2025-05-19 18:45:05 +0530

    frontend stacks

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div>
+      createpage
+    </div>
+  )
+}
+
+export default CreatePage
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+  return (
+    <div>
+      notepage
+    </div>
+  )
+}
+
+export default NoteDetailPage
