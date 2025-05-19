c3c82ccc5daab544e3cfb96d09e040b281270801
aniketsingh <aniketsingh2004@gmail.com>
2025-05-19 23:45:24 +0530

    final touch up

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors';
+
 import routesFile from './routes/Notes.route.js';
 import connectDB from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const app = express();
-
 app.use(express.json());
 app.use(rateLimiter);
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+    }
+));
+
+
 app.use("/api/notes",routesFile);
 
 
